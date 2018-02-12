1253e2d1a6937f06b141a1e6f42d06c9f9e867b2
csolisre <yukiko1993>
2018-02-12 17:28:33 +0100

    12 feb lynn/carlos(eerste pagina)

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<!--
+index.php
+-->
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="images/favico.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/favico.png">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>De wegwijzer | oever</title>
+</head>
+<body>
+<header>
+    <div class="top">
+        <div class="wraper">
+            <div class="top-container">
+                <div class="logo"><img src="images/logo-oeverdef.png"></div>
+                <div class="site-name"> De oever</div>
+            </div>
+        </div>
+    </div>
+
+</header>
+
+<div class="wraper">
+    <h1>De wegwijzer</h1>
+    <div class="node-container">
+
+        <div class="node1 item"><a href="#">To fsdfsfgsd change this license header, choose License Headers in Project
+                Properties.
+                To change this template file, choose Tools | Templates
+                and open the template in the editor.</a></div>
+        <div class="node2 item"><a href="#">To fsdfsfgsd change this license header, choose License Headers in Project
+                Properties.
+                To change this template file, choose Tools | Templates
+                and open the template in the editor.</a></div>
+        <div class="node3 item"><a href="#">To fsdfsfgsd change this license header, choose License Headers in Project
+                Properties.
+                To change this template file, choose Tools | Templates
+                and open the template in the editor.</a></div>
+        <div class="node4 item"><a href="#">To fsdfsfgsd change this license header, choose License Headers in Project
+                Properties.
+                To change this template file, choose Tools | Templates
+                and open the template in the editor.</a></div>
+    </div>
+</div>
+<?php
+// put your code here
+?>
+</body>
+</html>
