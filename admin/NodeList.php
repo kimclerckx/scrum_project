f26a0d9af4a8fe82ed612d0a4f0b1954bcf6fcc3
kim <kim.clerckx@gmail.com>
2018-02-13 15:59:10 +0100

    Admin tree added

---
+++ admin/NodeList.php
@@ -0,0 +1,41 @@
+<?php
+require_once 'Node.php';
+require_once '../Database.php';
+class NodeList
+{
+    public function getAllNodes()
+    {
+        $db = new Database();
+        $sql = "SELECT * FROM nodes WHERE ID != 1";
+        $db->executeWithoutParam($sql);
+        $resultSet = $db->resultset();
+        $db = null;
+        $nodeList = array();
+        foreach ($resultSet as $value) {
+            $content = array();
+            $content['ID'] = $value['ID'];
+            $content['content'] = $value['content'];
+            $content['parentID'] = $value['parentID'];
+            $content['button'] = $value['button'];
+            $content['hasChild'] = $value['hasChild'];
+            array_push($nodeList, $content);
+        }
+        return $nodeList;
+    }
+
+    function buildTree(array $elements, $parentID = 1)
+    {
+        $structure = "<ul>";
+        foreach ($elements as $element) {
+            if ($element['parentID'] == $parentID) {
+                $structure .= "<li>" . $element['content'];
+                if ($element['hasChild'] == 1) {
+                    $structure .= buildTree($elements, $element['ID']);
+                }
+                $structure .= "</li>";
+            }
+        }
+        $structure .= "</ul>";
+        return $structure;
+    }
+}
--- admin/loggedIn.php
+++ admin/loggedIn.php
@@ -1,5 +1,29 @@
 <?php
+require_once 'NodeList.php';
 session_start();
+
+function buildTree(array $elements, $parentID = 1)
+{
+    $structure = "<ul>";
+    foreach ($elements as $element) {
+        if ($element['parentID'] == $parentID) {
+            $structure .= "<li>" . $element['content']
+                . '<a href="NodeList.php?action=add&id=' . $element['ID'] . '"><i class="ion-plus-round"></i></a>'. ' '
+                . '<a href="NodeList.php?action=edit&id=' . $element['ID'] . '"><i class="ion-edit"></i></a>' . ' '
+                . '<a href="NodeList.php?action=delete&id=' . $element['ID'] . '"><i class="ion-close-round"></i></a>';
+            if ($element['hasChild'] == 1) {
+                $structure .= buildTree($elements, $element['ID']);
+            }
+            $structure .= "</li>";
+        }
+    }
+    $structure .= "</ul>";
+    return $structure;
+}
+$test = new NodeList();
+/**
+ * @var Node $node
+ */
 if (isset($_SESSION['email'])) {
     $welcome = ' Hello ' . $_SESSION['email'] . ' : you are now logged in.';
 }
@@ -13,16 +37,25 @@ if (isset($_SESSION['email'])) {
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/ionicons.min.css">
     <title>Document</title>
 </head>
 <body>
 <br><br>
 <div class="text-center">
-<?php
-print $welcome;
-?>
+    <?php
+    print $welcome;
+    ?>
+</div>
 <br/><br/>
-<a class="btn btn-primary" href="logout.php">Logout ?</a>
+<!-- Treeview -->
+<?php
+echo buildTree($test->getAllNodes());
+?>
+<!-- End of Treeview -->
+<br/><br/>
+<div class="text-center">
+    <a class="btn btn-primary" href="logout.php">Logout ?</a>
 </div>
 </body>
 </html>
