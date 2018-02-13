3f78d73df35b028c26ab983bdba99fe3dd33dd6e
kim <kim.clerckx@gmail.com>
2018-02-13 12:20:57 +0100

    Added bootstrap admin panel & login with logout function Can be found in the admin folder

--- admin/index.php
+++ admin/index.php
@@ -14,7 +14,9 @@ session_start();
 require_once '../Database.php';
 $db = new Database();
 $errors = [];
-
+if (isset($_SESSION['email'])) {
+    header("Location:loggedIn.php");
+}
 
 //LOGIN PROCESS//
 //1. Check is login button is clicked
@@ -70,22 +72,30 @@ if (isset($_POST['login'])) {
     <title>Admin Login</title>
 </head>
 <body>
-<form action="index.php" method="post">
-    <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-               placeholder="Enter email" name="email">
-        <small id="emailHelp" class="form-text text-muted">Please input your admin login email.</small>
+<br><br>
+<div class="container">
+    <div class="row">
+        <div class="col"></div>
+        <div class="col-4 text-center">
+            <form action="index.php" method="post">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                           placeholder="Enter email" name="email">
+                    <small id="emailHelp" class="form-text text-muted">Please input your admin login email.</small>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"
+                           name="password">
+                    <small id="emailHelp" class="form-text text-muted">Please input your admin login password.</small>
+                </div>
+                <button type="submit" class="btn btn-primary" name="login">Login</button>
+            </form>
+        </div>
+        <div class="col"></div>
     </div>
-    <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password">
-        <small id="emailHelp" class="form-text text-muted">Please input your admin login password.</small>
-    </div>
-    <button type="submit" class="btn btn-primary" name="login">Login</button>
-    <br/><br/>
-    <a class="btn btn-primary" href="logout.php">Logout ?</a>
-</form>
+</div>
 </body>
 </html>
 
