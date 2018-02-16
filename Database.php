867ba74107ac62e09696702d36719c9376224ddb
lynn <michiels89@hotmail.com>
2018-02-16 14:06:03 +0100

    added hover, active , visited effect buttons

--- Database.php
+++ Database.php
@@ -3,16 +3,11 @@
 class Database
 {
 
-  //  private $host = "172.30.24.7:3306";
-   // private $user = "fontonova";
-   // private $pass = "GumHM4Q4-pYgzWdYA";
-   // private $dbname = "fontonova";
-    
-    private $host = "localhost";
-    private $user = "root";
-    private $pass = "";
+    private $host = "172.30.24.7:3306";
+    private $user = "fontonova";
+    private $pass = "GumHM4Q4-pYgzWdYA";
     private $dbname = "fontonova";
-
+   
 
     private $dbh;
     private $error;
