1b51cdd5ab3499beb42902437646875d1f9e7c46
kim <kim.clerckx@gmail.com>
2018-02-20 14:08:17 +0100

    Log report half werkend

--- admin/logs.php
+++ admin/logs.php
@@ -3,17 +3,19 @@ require_once 'LogList.php';
 if(!isset($_SESSION)) 
 { 
     session_start(); 
-} 
-
-// Check if user is logged in
-if (!isset($_SESSION['email'])) {
-    header("Location:index.php");
-} else {
-    echo '<div class="header">';
-        echo "<p>Welkom {$_SESSION['email']} : je bent nu ingelogd als administrator.</p>";
-        echo "<a class='btn btn-primary' href='logout.php'>Logout</a>";
-    echo '</div>';
 }
+if(!isset($_GET['page']))
+{
+    $_GET['page'] = 1;
+}
+
+if (isset($_POST['aantLogs'])) {
+    $_SESSION['aantLogs'] = $_POST['aantLogs'];
+} else {
+    $_SESSION['aantLogs'] = 25;
+}
+$aantLogs = $_SESSION['aantLogs'];
+
 
 ?>
 <!DOCTYPE html>
@@ -23,34 +25,39 @@ if (!isset($_SESSION['email'])) {
     <title>Logs</title>
     <link rel="stylesheet" href="css/admin-style.css">
 </head>
-
 <body>
+<!--Check if user is logged in-->
+<?php
+if (!isset($_SESSION['email'])) {
+    header("Location:index.php");
+} else { ?>
+    <div class="header">
+        <p>Welkom <?= $_SESSION['email']?> : je bent nu ingelogd als administrator.</p>
+        <a class='btn btn-primary' href='logout.php'>Logout</a>
+    </div>
+<?php } ?>
+<!--Show log records-->
    <div>
         <form action="logs.php" method="post">
-
          <select name="aantLogs" id="aantLogs">
-         <option value="20" default>25</option>
-         <option value="50">50</option>
-         <option value="100">100</option>
-         <option value="150">150</option>
-        
+         <option value="25"<?php echo (isset($_POST['aantLogs']) && $_POST['aantLogs'] == '25')?'selected="selected"':''; ?> >25</option>
+             <option value="50"<?php echo (isset($_POST['aantLogs']) && $_POST['aantLogs'] == '50')?'selected="selected"':''; ?> >50</option>
+             <option value="75"<?php echo (isset($_POST['aantLogs']) && $_POST['aantLogs'] == '75')?'selected="selected"':''; ?> >75</option>
+             <option value="100"<?php echo (isset($_POST['aantLogs']) && $_POST['aantLogs'] == '100')?'selected="selected"':''; ?> >100</option>
          </select>
         <input type="submit">
         </form>
     </div>
    
-    
-    
-    
-
     <?php
-        $aantLogs = $_POST['aantLogs'];
         $ll = new LogList();
-        $list = $ll->getAllLogs($aantLogs);
-        $db = new Database();
-        $lastId = $db->lastInsertId();
-    
-    echo $lastId;
+        $logCount = $ll->getLogCount();
+
+    echo $logCount;
+    $aantPages = ceil($logCount/$aantLogs);
+    $list = $ll->getLogs($aantLogs, $_GET['page']);
+
+    echo '<br>' . $aantPages;
     
     ?>
        <div class="wrapperLogs">
@@ -82,6 +89,14 @@ if (!isset($_SESSION['email'])) {
     <?php  
         } 
     ?>
+
+           <?php
+           for ($i=1;$i <= $aantPages;$i++) {
+               echo "<a href='logs.php?page=" . $i . "'>". ($aantLogs*($i-1)). '-' . ($aantLogs*$i) ."</a>";
+           }
+           ?>
+
+
         </div>  
 </body>
 </html>
