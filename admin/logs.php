fa83a5c929f959081c4bc9d05182fae8cbaabcdc
kim <kim.clerckx@gmail.com>
2018-02-21 09:13:06 +0100

    Log file update after merge fuckup

--- admin/logs.php
+++ admin/logs.php
@@ -1,32 +1,17 @@
 <?php
 require_once 'LogList.php';
-<<<<<<< HEAD
-if(!isset($_SESSION)) 
-{ 
-    session_start(); 
-=======
 if (!isset($_SESSION)) {
     session_start();
->>>>>>> origin/master
 }
-<<<<<<< HEAD
-if(!isset($_GET['page']))
-{
-    $_GET['page'] = 1;
-}
-
-if (isset($_POST['aantLogs'])) {
-    $_SESSION['aantLogs'] = $_POST['aantLogs'];
-} else {
-    $_SESSION['aantLogs'] = 25;
-=======
 if (!isset($_GET['page'])) {
     $_GET['page'] = 1;
 }
+//if (!isset($_SESSION['firstLog'])) {
+//    $aantLogs = 25;
+//}
 
 if (isset($_POST['aantLogs'])) {
     $_SESSION['aantLogs'] = $_POST['aantLogs'];
->>>>>>> origin/master
 }
 $aantLogs = $_SESSION['aantLogs'];
 
@@ -46,53 +31,9 @@ if (!isset($_SESSION['email'])) {
     header("Location:index.php");
 } else { ?>
     <div class="header">
-<<<<<<< HEAD
-        <p>Welkom <?= $_SESSION['email']?> : je bent nu ingelogd als administrator.</p>
-        <a class='btn btn-primary' href='logout.php'>Logout</a>
-    </div>
-<?php } ?>
-<!--Show log records-->
-   <div>
-        <form action="logs.php" method="post">
-         <select name="aantLogs" id="aantLogs">
-         <option value="25"<?php echo (isset($_POST['aantLogs']) && $_POST['aantLogs'] == '25')?'selected="selected"':''; ?> >25</option>
-             <option value="50"<?php echo (isset($_POST['aantLogs']) && $_POST['aantLogs'] == '50')?'selected="selected"':''; ?> >50</option>
-             <option value="75"<?php echo (isset($_POST['aantLogs']) && $_POST['aantLogs'] == '75')?'selected="selected"':''; ?> >75</option>
-             <option value="100"<?php echo (isset($_POST['aantLogs']) && $_POST['aantLogs'] == '100')?'selected="selected"':''; ?> >100</option>
-         </select>
-        <input type="submit">
-        </form>
-=======
         <p>Welkom <?= $_SESSION['email'] ?> : je bent nu ingelogd als administrator.</p>
         <a class='btn btn-primary' href='logout.php'>Logout</a>
->>>>>>> origin/master
     </div>
-<<<<<<< HEAD
-   
-    <?php
-        $ll = new LogList();
-        $logCount = $ll->getLogCount();
-
-    echo $logCount;
-    $aantPages = ceil($logCount/$aantLogs);
-    $list = $ll->getLogs($aantLogs, $_GET['page']);
-
-    echo '<br>' . $aantPages;
-    
-    ?>
-       <div class="wrapperLogs">
-        
-           
-            <span class="box">#</span>
-            <span class="box">Start</span>
-            <span class="box">Einde</span>
-            <span class="box">Totale tijd</span>
-            <span class="box">Laatste element</span>       
-        
-    
-    <?php foreach($list as $log){ 
-    
-=======
 <?php } ?>
 <!--Show log records-->
 <div>
@@ -138,7 +79,6 @@ echo '<br>' . $aantPages;
 
     <?php foreach ($list as $log) {
 
->>>>>>> origin/master
         $datetime1 = new DateTime($log['timestampStart']);
         $datetime2 = new DateTime($log['timestampEnd']);
         $duurtijd = $datetime2->diff($datetime1);
@@ -159,17 +99,6 @@ echo '<br>' . $aantPages;
     }
     ?>
 
-<<<<<<< HEAD
-           <?php
-           for ($i=1;$i <= $aantPages;$i++) {
-               echo "<a href='logs.php?page=" . $i . "'>". ($aantLogs*($i-1)). '-' . ($aantLogs*$i) ."</a>";
-           }
-           ?>
-
-
-        </div>  
-=======
 </div>
->>>>>>> origin/master
 </body>
 </html>
