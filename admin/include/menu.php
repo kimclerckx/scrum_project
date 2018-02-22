a2caad8a5050d004340f485b312882922ee325b3
lynn <michiels89@hotmail.com>
2018-02-22 09:51:39 +0100

    excel button in menu 1/2 logs

--- admin/include/menu.php
+++ admin/include/menu.php
@@ -8,9 +8,9 @@ if(!isset($_SESSION)) {
 <div class="header text-center">
     <p>Welkom <?= $_SESSION['email'] ?>: je bent nu ingelogd als administrator.</p>
     <div>
-    <a class='btn btn-primary' href='logs.php'>Logs</a>
     <a class='btn btn-primary' href='contact.php' target='_self'>Pas contactgegevens aan </a>
     <a class='btn btn-primary' href='passwordChange.php'>Wijzig wachtwoord</a>
+    <a class='btn btn-primary' href='logs.php'>Logs</a>
     <a class='btn btn-primary' href='logout.php'>Logout</a>
     </div>
 </div>
