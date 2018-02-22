0cd8dde5c3c7846ee507940f116a770afb14969f
hipova <evgeniiatops@gmail.com>
2018-02-22 13:40:11 +0100

    Bewerk/Logs gefixed

--- admin/include/menu.php
+++ admin/include/menu.php
@@ -1,43 +1,29 @@
 <?php
-if(!isset($_SESSION)) {
-    session_start();
-}
+require_once ('../functions.php');
+
+    if(!isset($_SESSION)) {
+        session_start();
+    }
 
 ?>
 
-
-
-<!--  header menu-->
+<!-- Header menu -->
 
 <div class="header text-center">
     
     <p>Welkom <?= $_SESSION['email'] ?>: je bent nu ingelogd als administrator.</p>
    
-    <div><a class='btn btn-primary' href='contact.php' target='_self'>Pas contactgegevens aan </a></div>
+    <div><a class='btn btn-primary' href='contact.php'>Pas contactgegevens aan </a></div>
     <div><a class='btn btn-primary' href='passwordChange.php'>Wijzig wachtwoord</a></div>
     
-<!--     checking witch page we are on-->
-    
-    <script type="text/javascript">
-        var x = location.href;
-
-        <?php $url = "<script>document.write(x)</script>"?>   
-    </script>
-<!--    $url contains location of the page we are on, already tested-->
-    
-<!--    <?php echo $url;?>-->
-    
-    <?php if (strpos($url,'logs')){ ?>
-    <div><a class='btn btn-primary' href='loggedIn.php'>Bewerk</a></div>
- <?php   
-}else{  ?>
-    <div><a class='btn btn-primary' href='logs.php'>Logs</a></div>
-    <?php
-}
+   <?php 
+        if (strpos($_SERVER['REQUEST_URI'],'logs') > 1) { 
+            echo ("<div><a class='btn btn-primary' href='loggedIn.php'>Bewerk</a></div>");            
+        } else { 
+            echo ("<div><a class='btn btn-primary' href='logs.php'>Logs</a></div>");
+        }
     ?>
     
-    
-    <div><a class='btn btn-primary' href='logs.php'>Logs</a></div>
     <div><a class='btn btn-primary' href='logout.php'>Logout</a></div>
     <div>
     <form class="form-horizontal" action="exportToCSV.php" method="post" name="upload_excel"
