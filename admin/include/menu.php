f40d7f7866b6907e8f7e9061ccc1891ad4d835e8
Maxim <billiaumaxim@gmail.com>
2018-02-23 09:17:58 +0100

    Create new user

--- admin/include/menu.php
+++ admin/include/menu.php
@@ -9,10 +9,10 @@
 <!-- Header menu -->
 
 <div class="header text-center">
-    
+    <div class="logo"><img src="../images/logo-oeverdef.png"></div>
     <p>Welkom <span><?= $_SESSION['email'] ?></span>: je bent nu ingelogd als administrator.</p>
-   
-    <div><a class='btn btn-primary' href='contact.php'>Pas contactgegevens aan </a></div>
+    <div><a class='btn btn-primary' href='newUser.php'>Nieuwe gebruiker</a></div>
+    <div><a class='btn btn-primary' href='contact.php'>Pas contactgegevens aan</a></div>
     <div><a class='btn btn-primary' href='passwordChange.php'>Wijzig wachtwoord</a></div>
     
    <?php 
@@ -22,15 +22,15 @@
             echo ("<div><a class='btn btn-primary' href='logs.php'>Logs</a></div>");
         }
     ?>
-    
-    <div><a class='btn btn-primary' href='logout.php'>Logout</a></div>
-    <div>
     <form class="form-horizontal" action="exportToCSV.php" method="post" name="upload_excel"
           enctype="multipart/form-data">
         <div class="form-group">
                 <input type="submit" name="Export" class="btn btn-success" value="Exporteer naar Excel"/>
         </div>
     </form>
+    <div><a class='btn btn-primary' href='logout.php'>Logout</a></div>
+    <div>
+    
     
 </div>
 </div>
